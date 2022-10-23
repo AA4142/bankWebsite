f9e5f4ae42dcf0112abcb1908384bb2c92c4d41c
AA4142 <alexarmatas5@gmail.com>
2022-10-23 16:06:55 -0400

    Created navbar that moves on click, working on Credit Card Images

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const menuIcon = document.querySelector('.menu-icon');
-
+const headerMenu = document.querySelector('.headerMenuContainer');
 
 let menuOpen = false;
 
 menuIcon.addEventListener('click' , () => {
     if(!menuOpen){
         menuIcon.classList.add('open');
+        headerMenu.classList.add('open');
         menuOpen = true;
     } else{
         menuIcon.classList.remove('open');
+        headerMenu.classList.remove('open');
         menuOpen = false;
     }
 
