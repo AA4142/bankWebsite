b50a6c28b46cabef7cd62f42fe552bc66f613db2
AA4142 <alexarmatas5@gmail.com>
2022-10-28 13:48:08 -0400

    started working on react fromework, need to understand how to integrate on to my own website now

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+//we use route in order to define different routes in our applicatoin
+import { Route, Routes } from "react-router-dom";
 
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+//we import all the components we need in our app
+
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+
+const App = () => {
+    return(
+        <div>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<RecordList />} />
+                <Route path="/edit/:id" element={<Edit />} />
+                <Route path="/create" element={<Create />}/>
+
+            </Routes>
+        </div>
+    );
+};
+
+export default App;
