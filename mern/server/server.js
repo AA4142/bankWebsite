b47a93ffac772ff2a7556d75f637794f894bcaae
AA4142 <alexarmatas5@gmail.com>
2022-10-27 22:16:11 -0400

    began working on creating back end for bank website by initilizating Node and ExpressJS

---
+++ mern/server/server.js
@@ -0,0 +1,25 @@
+//require is a method(function) from NodeJS
+//using the require method  from NodeJS we get access to the 
+//ExpressJS is a module of NodeJS and we can load it
+//by using require("express"), we then call the variable
+//express to know we are working with the framework
+const express = require("express");
+
+//create the express application with the pulled module
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+//get driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () =>{
+    //perform a databse connection when server starts
+    dbo.connectToServer(function (err) {
+        if(err) console.error(err);
+    });
+    console.log(`Server is running on port: ${port}`)
+});
